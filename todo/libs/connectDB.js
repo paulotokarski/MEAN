524bd3d825fcc665dcb56ab65361756bc04ae885
Paulo Henrique Tokarski Glinski <paulotokarski@gmail.com>
2017-03-14 21:39:51 -0300

    Update connectDB.js

--- todo/libs/connectDB.js
+++ todo/libs/connectDB.js
@@ -3,7 +3,6 @@ var db;
 
 module.exports = function() {
   if(!db)
-    db = mongoose.connect('mongodb://paulotokarski:pass@ds139198.mlab.com:39198/mydb');
-
+    db = mongoose.connect('mongodb://<USER>:<PASSWORD>@<HOST>/<MYDB>');
   return db;
 }
